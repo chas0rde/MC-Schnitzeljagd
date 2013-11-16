7effeef519116da04e6236b0cfc4f3586699f0cf
sw87 <swahab@stud.hs-bremen.de>
2013-11-16 20:28:30 +0100

    Fehlende libs hinzugefügt und die Pfade geändert.

--- gen/de/hsb/kss/mc_schnitzeljagd/R.java
+++ gen/de/hsb/kss/mc_schnitzeljagd/R.java
@@ -24,13 +24,31 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int LinearLayout1=0x7f080002;
+        public static final int action_settings=0x7f080013;
+        public static final int conState=0x7f08000b;
+        public static final int conStateInfo=0x7f08000a;
+        public static final int count=0x7f08000d;
+        public static final int curLat=0x7f080005;
+        public static final int curLng=0x7f080007;
+        public static final int curQuality=0x7f080009;
+        public static final int fragment1=0x7f080012;
+        public static final int latInfo=0x7f080004;
+        public static final int listener=0x7f08000f;
+        public static final int listenerInfo=0x7f08000e;
+        public static final int lngInfo=0x7f080006;
+        public static final int map=0x7f080003;
+        public static final int quality=0x7f080008;
+        public static final int refresh=0x7f080010;
+        public static final int refreshInfo=0x7f08000c;
         public static final int textview=0x7f080000;
         public static final int toLocationTestActivity=0x7f080001;
+        public static final int toggleUpdates=0x7f080011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int locationtest=0x7f030001;
+        public static final int location_facade=0x7f030001;
+        public static final int locationtest=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -38,8 +56,16 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int conStateInfo=0x7f050007;
         public static final int hello_world=0x7f050002;
+        public static final int lat=0x7f050004;
+        public static final int listener=0x7f050009;
+        public static final int lng=0x7f050005;
         public static final int locationTest=0x7f050003;
+        public static final int quality=0x7f050006;
+        public static final int refresh=0x7f05000a;
+        public static final int toggleUpdates=0x7f05000b;
+        public static final int updates=0x7f050008;
     }
     public static final class style {
         /** 
