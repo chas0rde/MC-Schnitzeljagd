52540e3a4901b6f98982e778d4aeb40a3bf2fa2e
Markus Halm <markus.halm@googlemail.com>
2013-11-18 16:38:27 +0100

    gg

--- src/de/hsb/kss/mc_schnitzeljagd/ui/HintActivity.java
+++ src/de/hsb/kss/mc_schnitzeljagd/ui/HintActivity.java
@@ -12,8 +12,8 @@ public class HintActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_hint);
-		// mein code
+		setContentView(R.layout.activity_hint);		
+		//fasg
 	}
 
 	@Override
