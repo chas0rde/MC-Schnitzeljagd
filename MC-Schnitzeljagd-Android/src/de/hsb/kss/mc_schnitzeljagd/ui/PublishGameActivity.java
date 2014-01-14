67e8def007fc4cb6481b84b46ab138906a07f7ba
toadette <melanie.loebner@gmx.de>
2014-01-14 22:41:43 +0100

    make ImageView for sharing clickable added shareFunction for Twitter

--- MC-Schnitzeljagd-Android/src/de/hsb/kss/mc_schnitzeljagd/ui/PublishGameActivity.java
+++ MC-Schnitzeljagd-Android/src/de/hsb/kss/mc_schnitzeljagd/ui/PublishGameActivity.java
@@ -3,6 +3,7 @@ package de.hsb.kss.mc_schnitzeljagd.ui;
 import de.hsb.kss.mc_schnitzeljagd.R;
 import de.hsb.kss.mc_schnitzeljagd.R.layout;
 import de.hsb.kss.mc_schnitzeljagd.R.menu;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -15,36 +16,57 @@ public class PublishGameActivity extends SchnitzelActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_publish_game);	
-		boolean gameSaved=false;
-		if(app != null)
-		{
-			gameSaved=app.getGameCreation().save();
+		setContentView(R.layout.activity_publish_game);
+		boolean gameSaved = false;
+		if (app != null) {
+			gameSaved = app.getGameCreation().save();
 		}
 		initUi(gameSaved);
 	}
-	
-	protected void initUi(boolean gameSaved)
-	{
+
+	protected void initUi(boolean gameSaved) {
 		super.initUi();
-		TextView code = (TextView)findViewById(R.id.share_access_code_id); 
-		if(code != null && app != null)
-		{
-			code.setText(gameSaved+" "+app.getGameCreation().getCurrentQuest().getAccessCode());
+		TextView code = (TextView) findViewById(R.id.share_access_code_id);
+		if (code != null && app != null) {
+			code.setText(gameSaved + " "
+					+ app.getGameCreation().getCurrentQuest().getAccessCode());
 		}
 	}
 
-	public void backToMainActivity(View view){
+	public void backToMainActivity(View view) {
 		Intent goBackToMain = new Intent(this, MainActivity.class);
 		startActivity(goBackToMain);
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.publish_game, menu);
 		return true;
+	}
+
+	public void shareCodeTwitter(View view) {
+		String tweetUrl = "https://twitter.com/intent/tweet?text=";
+		String message= "Spiele eine Schnitzeljagd mit mir ! Zugangscode:";
+		String code=app.getGameCreation().getCurrentQuest().getAccessCode();
+		Uri uri = Uri.parse(tweetUrl+message+code);
+		startActivity(new Intent(Intent.ACTION_VIEW, uri));
+	}
+
+	public void shareCodeFacebook(View view) {
+		String facebookurl="https://www.facebook.com/sharer/sharer.php?u=https://github.com/chas0rde/MC-Schnitzeljagd";
+		String message= "Spiele eine Schnitzeljagd mit mir ! Zugangscode:";
+		String code=app.getGameCreation().getCurrentQuest().getAccessCode();
+//		Uri uri = Uri.parse(tweetUrl+message+code);
+//		startActivity(new Intent(Intent.ACTION_VIEW, uri));
+	}
+
+	public void shareCodePinterest(View view) {
+
+	}
+
+	public void shareCodeEmail(View view) {
+
 	}
 
 }
