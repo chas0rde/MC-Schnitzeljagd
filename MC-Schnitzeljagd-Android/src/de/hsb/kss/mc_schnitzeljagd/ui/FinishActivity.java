50b320b41e27abdee9f075002bf29d7df936d704
sw87 <swahab@stud.hs-bremen.de>
2014-02-16 16:53:56 +0100

    no message

--- MC-Schnitzeljagd-Android/src/de/hsb/kss/mc_schnitzeljagd/ui/FinishActivity.java
+++ MC-Schnitzeljagd-Android/src/de/hsb/kss/mc_schnitzeljagd/ui/FinishActivity.java
@@ -5,7 +5,9 @@ import de.hsb.kss.mc_schnitzeljagd.R.layout;
 import de.hsb.kss.mc_schnitzeljagd.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class FinishActivity extends Activity {
 
@@ -20,6 +22,11 @@ public class FinishActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.finish, menu);
 		return true;
+	}
+	
+	public void backToMain(View view) {
+		Intent goBackToMain = new Intent(this, MainActivity.class);
+		startActivity(goBackToMain);
 	}
 
 }
